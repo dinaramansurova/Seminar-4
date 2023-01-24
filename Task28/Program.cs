544a654284c408463b0935c8a08b1c38a9c5ce5c
Dinara <ash.solarium@gmail.com>
2023-01-24 11:58:08 +0300

    over

---
+++ Task28/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число N и выдает произведение чисел от 1 до N
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int sum = 1;
+
+for (int i = 2; i <= n; i++)
+{
+    sum = sum * i;
+}
+Console.WriteLine($"{sum}");
